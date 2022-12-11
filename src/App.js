5c410a3c25a95ae81bb813e553ba7757dfacda51
dung92 <dung92@users.noreply.github.com>
2022-12-12 06:50:29 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import EditVideo from './page/EditVideo';
+
+function App() {
+  return (
+    <div className="App">
+      <EditVideo/>
+    </div>
+  );
+}
+
+export default App;
